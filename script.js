f0790dd9e4d9ccae27dc8e5fadd8cb7e6380d61c
James Lovallo <lovallo.james@gmail.com>
2026-06-17 11:47:51 -0400

    Refactor message assignment logic in script.js

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ const D = new Date()
 
 const [month, day] = [D.getMonth() + 1, D.getDay() + 1]
 
-let message
+let message = "Nope"
 
-if (month !== 12) {
-  message = "Nope."
-} else if (day === 2) {
-  message = "Almost!"
-} else if (day >= 3 || day <= 5) {
-  message = "YES!!!!!!!!!!!"
+if (month === 12) {
+  if (day === 2) message = "Almost!"
+  if (day >= 3 && day <= 5) message = "YES!!!!!!!!"
 }
 
 document.body.innerHTML += `<h1>${message}</h1>`
